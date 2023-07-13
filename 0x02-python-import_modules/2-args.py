3415b463f6b28f726bdbb950971b7d2affa53541
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-13 06:05:34 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if (argc == 0):
+        print("0 arguments.")
+    elif (argc == 1):
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+    for c in range(argc):
+        print("{} : {}".format(c + 1, sys.argv[c + 1]))
