1675269b62cad9f6e73ece71be482e046d2f947e
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-15 06:30:55 -0700

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 def replace_in_list(my_list, idx, element):
     length = len(my_list) - 1
     if (idx < 0):
-        return "None"
+        return my_list
     elif (idx > length):
-        return "None"
+        return my_list
     else:
         my_list[idx] = element
         return my_list
