ecf6cc35e82e18d4c90fdc80296ebe379cb3d277
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-19 13:25:44 -0700

    ad

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,13 @@ def roman_to_int(roman_string):
     if not roman_string or len(roman_string) == 0 or type(roman_string) != str:
         return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_num = 0
-    length = len(roman_string)
-    for i in range(length):
-        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
-            roman_num = roman[roman_string[i]] - 2 * \
-                        roman[roman_string[i - 1]] + roman_num
+    total = 0
+    prev = 0
+    for i in reversed(roman_string):
+        val = roman.get(i, 0)
+        if val < prev:
+            total = total - val
         else:
-            roman_num = roman[roman_string[i]] + roman_num
-    return roman_num
+            total = total + val
+        prev = val
+    return total
