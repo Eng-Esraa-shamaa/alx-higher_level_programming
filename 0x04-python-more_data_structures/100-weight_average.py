20422a6b2b11aeb24f06ae03df985efb3307d744
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-19 12:42:38 -0700

    adv

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return 0
+    avg = 0
+    div = 0
+    for tupl in my_list:
+        avg = (tupl[0] * tupl[1]) + avg
+        div = tupl[1] + div
+    total = float(avg / div)
+    return total
+        
