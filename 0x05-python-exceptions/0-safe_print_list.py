79a87c7861a1e6e54ddd776c9ea169c620994253
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-31 03:25:13 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             result += 1
-            if result >= x:
-                break
         except IndexError:
             break
     print("")
