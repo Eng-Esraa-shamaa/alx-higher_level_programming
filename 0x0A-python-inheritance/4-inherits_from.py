ddfdb1960962ce14ab659b6ecdafcd45fc0d2636
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-14 10:02:33 -0700

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return:
+        true or false
+    Args:
+        obj: the object to test
+        a_class: the class to compare
+    """
+    if (issubclass(type(obj), a_class)) and type(obj) != a_class:
+        return True
+    if type(obj) == a_class:
+        return False
