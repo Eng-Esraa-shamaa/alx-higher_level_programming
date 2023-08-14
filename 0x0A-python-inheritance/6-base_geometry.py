71e338884a6bb0345089dafc6f2d6d1e90bfcde2
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-14 10:17:21 -0700

    jjj

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    a class BaseGeometrywith exception
+    """
+    def area(self):
+        """area with exception"""
+        raise Exception("area() is not implemented")
