37bc0374b2a8c4e291051e922503d02c3585762c
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-14 10:06:38 -0700

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,5 +16,4 @@ def inherits_from(obj, a_class):
     """
     if (issubclass(type(obj), a_class)) and type(obj) != a_class:
         return True
-    elif type(obj) == a_class:
-        return False
+    return False
