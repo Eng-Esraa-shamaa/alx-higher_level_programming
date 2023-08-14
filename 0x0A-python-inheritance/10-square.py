1cc542c151e14535348c0c38bfa39d1f320982ae
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-14 11:36:04 -0700

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""super class Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square."""
+
+    def __init__(self, size):
+        """
+        Args:
+            size: size of square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area of square"""
+        return (self.__size * self.__size)
