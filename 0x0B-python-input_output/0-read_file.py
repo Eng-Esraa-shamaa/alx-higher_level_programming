0b15e2f4eb290515dfa5e7e29459e3b88c0bdb77
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-15 07:38:05 -0700

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    reads a file
+    """
+    with open(filename, 'r', encoding="UTF-8") as f:
+        print(f.read(), end="")
