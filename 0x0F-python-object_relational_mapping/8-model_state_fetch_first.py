ee29f52fdb0719a24d6407edf8226910b2841001
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-10-19 15:25:20 -0700

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all State objects from the database hbtn_0e_6_usa
+ a script that prints the first State object from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -10,11 +10,14 @@ from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            'mysql+mysqldb://{}:{}@localhost:3306/{}'
                             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping=True
                            )
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    first = session.query(State).order_by(State.id).first()
+    if first is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first.id, first.name))
     session.close()
